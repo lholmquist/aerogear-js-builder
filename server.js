3a78a2d3108547ae5d44970647f8b63895f029aa
Lucas Holmquist <lholmqui@redhat.com>
2013-01-04 11:36:05 -0500

    code cleanup

--- server.js
+++ server.js
@@ -3,19 +3,10 @@
 var _ = require( 'underscore' ),
     express = require( 'express' ),
     app = express.createServer(),
-    async = require( 'async'),
     crypto = require( 'crypto' ),
-    cssConcat = require( 'css-concat' ),
-    fetch = require( './lib/project' ).fetch,
     fs = require( 'fs' ),
     mime = require( 'mime' ),
     path = require( 'path' ),
-    promiseUtils = require( 'node-promise' ),
-    Promise = require( 'node-promise').Promise,
-    when = require( 'node-promise').when,
-    regexp = require( './lib/regexp' ),
-    requirejs = require( 'requirejs' ),
-    semver = require( 'semver' ),
     url = require( 'url' ),
     zip = require("node-native-zip" ),
     rimraf = require( "rimraf" );
@@ -27,14 +18,6 @@ var zcache = { 'index.html': '','builder.html':'', 'banner':"'<banner:meta.banne
 zcache['index.html'] = fs.readFileSync('./index.html'); //  Cache index.html
 zcache['builder.html'] = fs.readFileSync( "./builder.html" );
 
-var Project = require( './lib/project' )
-    .repoDir( "" )
-    .stagingDir( dataDir + "aerogear-js-stage/" )
-    .Project,
-  filters = {},
-  bundlePromises = {},
-  dependenciesPromises = {};
-
 app.use(express.bodyParser());
 
 // Create "express" server.
@@ -45,321 +28,38 @@ var app  = express.createServer();
 /*  Setup route handlers.  */
 /*  =====================================================================  */
 
-var bid = 0;
-
-function applyFilter( baseUrl, filter, contents, ext, callback ) {
-    if ( filter ) {
-        require( path.join( baseUrl, filter ) )( contents, ext, callback );
-    } else {
-        callback( null, contents );
-    }
-}
-
-var bjsid = 0;
-function buildJSBundle( project, config, name, filter, optimize ) {
-    var id = bjsid ++;
-//    console.log( "buildJSBundle["+id+"]()" );
-    var promise = new Promise(),
-        baseUrl = config.baseUrl,
-        wsDir = project.getWorkspaceDirSync(),
-        ext = ( optimize ? ".min" : "" ) + ".js",
-        out = path.join( project.getCompiledDirSync(), name + ext );
-
-    fs.exists( out, function ( exists ) {
-        if ( exists ) {
-            console.log( "buildJSBundle: resolving promise" );
-            promise.resolve( out );
-        } else {
-            async.waterfall([
-                function( next ) {
-                    console.log( "buildJSBundle["+id+"](): step 1" );
-                    var outDir = path.dirname( config.out );
-                    console.log( "mkdir '" + outDir + "'" );
-                    fs.mkdir( outDir, function( err ) {
-                        console.log(err);
-                        if ( err && err.code != "EEXIST" ) {
-                            next( err );
-                        } else {
-                            next();
-                        }
-                    });
-                },
-                function( next ) {
-                    console.log( "buildJSBundle["+id+"](): step 2" );
-                    try {
-                        requirejs.optimize(
-                            _.extend({
-                                out: out,
-                                optimize: ( optimize ? "uglify" : "none" )
-                            }, config ),
-                            function( response ) {
-                                next( null, response );
-                            }
-                        );
-                    } catch ( e ){
-                        next( e.toString() );
-                    }
-                },
-                function( response, next ) {
-                    console.log( "buildJSBundle["+id+"](): step 3" );
-                    fs.readFile( out, 'utf8', next );
-                },
-                function ( contents, next ) {
-                    console.log( "buildJSBundle["+id+"](): step 4" );
-                    applyFilter( baseUrl, filter, contents, ext, next );
-                },
-                function( contents, next ) {
-                    fs.writeFile( out, contents, 'utf8', next );
-                }
-            ], function( err ) {
-                if( err ) {
-                    promise.reject( err );
-                } else {
-//                    console.log( "buildJSBundle: resolving promise" );
-                    promise.resolve( out );
-                }
-            });
-        }
-    });
-    return promise;
-}
-
-function buildZipBundle( project, name, config, digest, filter )  {
-    console.log( "buildZipBundle()" );
-    var promise = new Promise(),
-        baseUrl = config.baseUrl,
-        basename = path.basename( name, ".zip" ),
-        out = path.join( project.getCompiledDirSync(), digest + ".zip" );
-
-    fs.exists( out, function ( exists ) {
-        if ( exists ) {
-            promise.resolve( out );
-        } else {
-            promiseUtils.all([
-                buildJSBundle( project, config, digest, filter ),
-                buildJSBundle( project, config, digest, filter, true )
-            ]).then(
-                function( results ) {
-                    var archive = new zip();
-
-                    async.series([
-                        function( next ) {
-                            async.forEachSeries( results, function( bundle, done ) {
-                                var nameInArchive;
-                                if ( typeof( bundle ) === "string" ) {
-                                    nameInArchive = path.basename( bundle ).replace( digest, name.substring( 0, name.lastIndexOf( "." )) );
-                                    archive.addFiles( [{ name: nameInArchive, path: bundle }], done );
-                                } else {
-                                    archive.addFiles(
-                                        bundle.map( function( file ) {
-                                            var nameInArchive = path.basename( file ).replace( digest, name.substring( 0, name.lastIndexOf( "." )) );
-                                            return( { name: nameInArchive, path: file } );
-                                        }), done
-                                    );
-                                }
-                            }, next );
-                        },
-                        function( next ) {
-                            fs.writeFile( out, archive.toBuffer(), next );
-                        }
-                    ], function( err ) {
-                        if( err ) {
-                            promise.reject( err );
-                        } else {
-                            promise.resolve( out );
-                        }
-                    });
-                }
-            );
-        }
-    });
-    return promise;
-}
-
-app.get( '/v1/bundle/:owner/:repo/:ref/:name?', function ( req, res ) {
-  console.log( "Building bundle for " + req.params.owner + "/" + req.params.repo + " ref: " + req.params.ref );
-    var project = new Project( req.params.owner, req.params.repo, req.params.ref ),
-        include = req.param( "include", "main" ).split( "," ).sort(),
-        exclude = req.param( "exclude", "" ).split( "," ).sort(),
-        optimize = Boolean( req.param( "optimize", false ) ).valueOf(),
-        wrapParam = req.param( "wrap" ),
-        wrap = wrapParam?JSON.parse( wrapParam ) : undefined,
-        pragmas = JSON.parse( req.param( "pragmas", "{}" ) ),
-        pragmasOnSave = JSON.parse( req.param( "pragmasOnSave", "{}" ) ),
-        name = req.params.name || ( req.params.repo + ".js" ),
-        ext = (optimize !== "none" ? ".min" : "") + ( path.extname( name ) || ".js" ),
-        mimetype = mime.lookup( ext ),
-        filter = req.param( "filter" ),
-        shasum = crypto.createHash( 'sha1' ),
-        wsDir   = project.getWorkspaceDirSync(),
-        baseUrl = wsDir,
-        dstDir, dstFile, digest, hash;
-
-    // var baseUrlFilters[baseUrl] = require(path.join(baseUrl, 'somemagicnameOrpackage.jsonEntry.js'));
-  var config = {
-    baseUrl: baseUrl,
-    include: include,
-        exclude: exclude,
-        wrap: wrap,
-        pragmas: pragmas,
-        pragmasOnSave: pragmasOnSave,
-        skipModuleInsertion: req.param( "skipModuleInsertion", "false" ) === "true" ,
-        preserveLicenseComments: req.param( "preserveLicenseComments", "true" ) === "true"
-  };
-    shasum.update( JSON.stringify( config ) );
-    shasum.update( mimetype );
-    if ( filter ) {
-        shasum.update( filter );
-    }
-
-    if ( mimetype === "application/zip" ) {
-        // For the zip file, the name needs to be part of the hash because it will determine the name of the files inside the zip file
-        shasum.update( name );
-    }
-
-    digest = shasum.digest( 'hex' );
-
-    if ( mimetype === "application/zip" ) {
-        hash = digest;
-    } else {
-        hash += ( optimize ? ".min" : "" );
-    }
-
-    function onBundleBuildError( error ) {
-        console.log(error);
-        res.header( "Access-Control-Allow-Origin", "*");
-        res.send( error, 500 );
-        delete bundlePromises[ digest ];
-    }
-
-    function buildBundle() {
-        var hash = digest;
-        if ( mimetype === "application/zip" ) {
-            bundlePromises[ hash ] = buildZipBundle( project, name, config, digest, filter );
-        } else if ( mimetype === "text/css" ) {
-            bundlePromises[ hash ] = buildCSSBundles( project, config, digest, filter, optimize );
-        } else {
-            bundlePromises[ hash ] = buildJSBundle( project, config, digest, filter, optimize );
-        }
-        bundlePromises[ hash ].then( onBundleBuilt, onBundleBuildError );
-    }
-
-    function onBundleBuilt( bundle ) {
-        var out,
-            promise = new Promise();
-
-        // Set up our promise callbacks
-        promise.then(
-            function( bundleInfo ) {
-                res.header( "Access-Control-Allow-Origin", "*");
-                res.download( bundleInfo.path, bundleInfo.name );
-            },
-            function() {
-                // Try to land back on our feet if for some reasons the built bundle got cleaned up;
-                delete bundlePromises[ hash ];
-                buildBundle();
-            }
-        );
-
-        if ( typeof( bundle ) === "string" ) {
-            fs.exists( bundle, function ( exists ) {
-                if ( exists ) {
-                    promise.resolve( { path: bundle, name: name } );
-                } else {
-                    promise.reject();
-                }
-            });
-        } else {
-            out = path.join( project.getCompiledDirSync(), digest + ext + ".zip" );
-            fs.exists( out, function ( exists ) {
-                var archive;
-                if ( exists ) {
-                    promise.resolve( { path: out, name: name } );
-                } else {
-                    archive = new zip();
-                    async.series([
-                        function( next ) {
-                            archive.addFiles(
-                                bundle.map( function( file ) {
-                                    var nameInArchive = path.basename( file ).replace( digest, name.substring( 0, name.lastIndexOf( "." )) );
-                                    return( { name: nameInArchive, path: file } );
-                                }),
-                                next
-                            );
-                        },
-                        function( next ) {
-                           fs.writeFile( out, archive.toBuffer(), next );
-                        }
-                    ],
-                    function( err ) {
-                        if( err ) {
-                            promise.reject();
-                        } else {
-                            promise.resolve( { path: out, name: name + ".zip" } );
-                        }
-                    });
-               }
-            });
-        }
-    }
-
-    if ( !bundlePromises[ hash ] ) {
-        buildBundle();
-    } else {
-        bundlePromises[ hash ].then( onBundleBuilt, onBundleBuildError );
-    }
-});
 
 app.get( '/aerogearjsbuilder/bundle/:owner/:repo/:ref/:name?', function ( req, res ) {
-    var project = new Project( req.params.owner, req.params.repo, req.params.ref ),
-        include = req.param( "include", "main" ).split( "," ).sort(),
+    var include = req.param( "include", "main" ).split( "," ).sort(),
         exclude = req.param( "exclude", "" ).split( "," ).sort(),
         optimize = Boolean( req.param( "optimize", false ) ).valueOf(),
         name = req.params.name || ( req.params.repo + ".js" ),
         ext = (optimize !== "none" ? ".min" : "") + ( path.extname( name ) || ".js" ),
         mimetype = mime.lookup( ext ),
         shasum = crypto.createHash( 'sha1' ),
-        wsDir   = project.getWorkspaceDirSync(),
-        baseUrl = wsDir,
         dstDir, dstFile, digest, hash;
 
-    // var baseUrlFilters[baseUrl] = require(path.join(baseUrl, 'somemagicnameOrpackage.jsonEntry.js'));
     var config = {
-        baseUrl: baseUrl,
         include: include,
         exclude: exclude
     };
     shasum.update( JSON.stringify( config ) );
     shasum.update( mimetype );
 
-    if ( mimetype === "application/zip" ) {
-        // For the zip file, the name needs to be part of the hash because it will determine the name of the files inside the zip file
-        shasum.update( name );
-    }
-
     digest = shasum.digest( 'hex' );
 
-    //if ( mimetype === "application/zip" ) {
-        hash = digest;
-    //} else {
-    //    hash += ( optimize ? ".min" : "" );
-   // }
+    hash = digest;
 
     var directoryDate = Date.now();
     fs.mkdir( dataDir + directoryDate + "/", 0755, function( err ) {
         if( err ) {
-            console.log( err );
-            errorReponse( res );
+            errorReponse( res, err );
             throw err;
         }
-
-        console.log( "dir created" );
-
         //Begin ReadFile
         fs.readFile( dataDir + "gruntbase.js","utf-8", function( err, data) {
             if( err ) {
-                console.log( "gruntbase"+err );
-                errorReponse( res );
+                errorReponse( res, err );
             }
             //build replacement
             var replacement = "[" + zcache[ "banner" ] + ", ";
@@ -376,8 +76,7 @@ app.get( '/aerogearjsbuilder/bundle/:owner/:repo/:ref/:name?', function ( req, r
             fs.writeFile( dataDir + directoryDate + "/" + hash + ".js", temp, "utf8", function( err ) {
 
                 if( err ) {
-                    console.log( "oh snap" + err);
-                    errorReponse( res );
+                    errorReponse( res, err );
                     throw err;
                 }
 
@@ -403,8 +102,7 @@ app.get( '/aerogearjsbuilder/bundle/:owner/:repo/:ref/:name?', function ( req, r
                         { name: "aerogear." + hash + ".js", path: dataDir + directoryDate + "/aerogear." + hash + ".js" }
                     ], function( err ) {
                         if( err ) {
-                            console.log( err );
-                            errorResponse( res );
+                            errorResponse( res, err );
                         }
                         var buff = archive.toBuffer();
 
@@ -426,9 +124,9 @@ app.get( '/aerogearjsbuilder/bundle/:owner/:repo/:ref/:name?', function ( req, r
     });
 
 
-    function errorReponse( res ) {
+    function errorReponse( res, err ) {
         res.status( 500 );
-        res.send( "error" );
+        res.send( err );
     }
 
 });
