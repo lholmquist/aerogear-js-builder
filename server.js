6313005c5948166bfe8caf480f2c26a322eed487
Lucas Holmquist <lholmqui@redhat.com>
2013-11-21 10:58:08 -0500

    Update to new node 0.10 cart

--- server.js
+++ server.js
@@ -31,177 +31,245 @@ var dataDir = process.env.OPENSHIFT_DATA_DIR ? process.env.OPENSHIFT_DATA_DIR + 
     repoDir = process.env.OPENSHIFT_REPO_DIR ? process.env.OPENSHIFT_REPO_DIR : "../aerogear-js-builder/",
     sourceMapPrefix = process.env.OPENSHIFT_REPO_DIR ? "9" : "4";
 
-app.use(express.bodyParser());
 
-// Create "express" server.
-var app  = express.createServer();
+/**
+ *  Define the sample application.
+ */
+var SampleApp = function() {
+
+    //  Scope.
+    var self = this;
 
 
-/*  =====================================================================  */
-/*  Setup route handlers.  */
-/*  =====================================================================  */
-app.get( "/builder/deps", function( request, response ) {
-    var callback = request.query.callback || request.query.jsonp,
-        responseBody = "",
-        packageJSON = require( dataDir + "package.json" ),
-        aerogearJSON = require( "./js/aerogear.json" );
+    /*  ================================================================  */
+    /*  Helper functions.                                                 */
+    /*  ================================================================  */
 
-        if( packageJSON.version ) {
-            aerogearJSON.version.version = packageJSON.version;
+    /**
+     *  Set up server IP address and port # using env variables/defaults.
+     */
+    self.setupVariables = function() {
+        //  Set the environment variables we need.
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+        if (typeof self.ipaddress === "undefined") {
+            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+            //  allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+            self.ipaddress = "127.0.0.1";
         }
-
-        responseBody = JSON.stringify( aerogearJSON );
-
-        if( callback ) {
-            response.send( callback + "(" + responseBody + " ) " );
-        } else {
-            response.send( responseBody );
-        }
-});
-
-app.get( '/builder/bundle/:owner/:repo/:ref/:name?', function ( req, res ) {
-    var include = req.param( "include", "main" ).split( "," ),
-        exclude = req.param( "exclude", "" ).split( "," ),
-        external = req.param( "external", "" ).split( "," ),
-        optimize = Boolean( req.param( "optimize", false ) ).valueOf(),
-        name = req.params.name || ( req.params.repo + ".js" ),
-        ext = (optimize !== "none" ? ".min" : "") + ( path.extname( name ) || ".js" ),
-        mimetype = mime.lookup( ext ),
-        shasum = crypto.createHash( 'sha1' ),
-        dstDir, dstFile, digest, hash;
-
-    if( external[0].length ) {
-        include = external.concat( include );
-    }
-    var config = {
-        include: include,
-        exclude: exclude
     };
-    shasum.update( JSON.stringify( config ) );
-    shasum.update( mimetype );
 
-    hash = shasum.digest( 'hex' );
-
-    var directoryDate = Date.now();
-    fs.mkdir( tempSaveDir + directoryDate + "/", 0755, function( err ) {
-        if( err ) {
-            errorResponse( res, err );
-            throw err;
+    /**
+     *  terminator === the termination handler
+     *  Terminate server on receipt of the specified signal.
+     *  @param {string} sig  Signal to terminate on.
+     */
+    self.terminator = function(sig){
+        if (typeof sig === "string") {
+           console.log('%s: Received %s - terminating sample app ...',
+                       Date(Date.now()), sig);
+           process.exit(1);
         }
-        //Begin ReadFile
-        fs.readFile( "gruntbase.js","utf-8", function( err, data) {
-            if( err ) {
-                errorResponse( res, err );
-            }
-            //build replacement
-            var replacement = "[ ";
-            _.each( config.include, function( val, index, list ) {
-                replacement += "'" + val + ".js'";
-                if( (index+1) !== list.length ) {
-                    replacement += ", ";
+        console.log('%s: Node server stopped.', Date(Date.now()) );
+    };
+
+
+    /**
+     *  Setup termination handlers (for exit and a list of signals).
+     */
+    self.setupTerminationHandlers = function(){
+        //  Process on exit and signals.
+        process.on('exit', function() { self.terminator(); });
+
+        // Removed 'SIGPIPE' from the list - bugz 852598.
+        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+        ].forEach(function(element, index, array) {
+            process.on(element, function() { self.terminator(element); });
+        });
+    };
+
+
+    /*  ================================================================  */
+    /*  App server functions (main app logic here).                       */
+    /*  ================================================================  */
+
+    /**
+     *  Create the routing table entries + handlers for the application.
+     */
+
+
+    /**
+     *  Initialize the server (express) and create the routes and register
+     *  the handlers.
+     */
+    self.initializeServer = function() {
+        self.app = express.createServer();
+
+        self.app.get( "/builder/deps", function( request, response ) {
+            var callback = request.query.callback || request.query.jsonp,
+                responseBody = "",
+                packageJSON = require( dataDir + "package.json" ),
+                aerogearJSON = require( "./js/aerogear.json" );
+
+                if( packageJSON.version ) {
+                    aerogearJSON.version.version = packageJSON.version;
                 }
-            });
 
-            replacement += "]";
-            var temp = data.replace("\"@SRC@\"", replacement)
-                           .replace("\"@DEST@\"", "'" + tempSaveDir + directoryDate + "/<%= pkg.name %>.custom.js'" )
-                           .replace("\"@DESTIIFE@\"", "'" + tempSaveDir + directoryDate + "/aerogear.custom.js'" )
-                           .replace( "\"@DESTMIN@\"",  "'" + tempSaveDir + directoryDate + "/<%= pkg.name %>.custom.min.js'" )
-                           .replace( "\"@DESTSOURCEMAP@\"", "'" + tempSaveDir + directoryDate + "/<%= pkg.name %>.custom.map'" )
-                           .replace( "\"@CONCAT@\"", "'" + repoDir + "node_modules/grunt-contrib-concat/tasks'")
-                           .replace( "\"@UGLY@\"", "'" + repoDir + "node_modules/grunt-contrib-uglify/tasks'")
-                           .replace( "\"@SOURCEMAPNAME@\"", "'<%= pkg.name %>.custom.min.js'")
-                           .replace( "\"@SOURCEMAPPREFIX@\"", sourceMapPrefix );
-            //write a new temp grunt file
-            fs.writeFile( tempSaveDir + directoryDate + "/" + hash + ".js", temp, "utf8", function( err ) {
+                responseBody = JSON.stringify( aerogearJSON );
 
+                if( callback ) {
+                    response.send( callback + "(" + responseBody + " ) " );
+                } else {
+                    response.send( responseBody );
+                }
+        });
+
+        self.app.get( '/builder/bundle/:owner/:repo/:ref/:name?', function ( req, res ) {
+            var include = req.param( "include", "main" ).split( "," ),
+                exclude = req.param( "exclude", "" ).split( "," ),
+                external = req.param( "external", "" ).split( "," ),
+                optimize = Boolean( req.param( "optimize", false ) ).valueOf(),
+                name = req.params.name || ( req.params.repo + ".js" ),
+                ext = (optimize !== "none" ? ".min" : "") + ( path.extname( name ) || ".js" ),
+                mimetype = mime.lookup( ext ),
+                shasum = crypto.createHash( 'sha1' ),
+                dstDir, dstFile, digest, hash;
+
+            if( external[0].length ) {
+                include = external.concat( include );
+            }
+            var config = {
+                include: include,
+                exclude: exclude
+            };
+            shasum.update( JSON.stringify( config ) );
+            shasum.update( mimetype );
+
+            hash = shasum.digest( 'hex' );
+
+            var directoryDate = Date.now();
+            fs.mkdir( tempSaveDir + directoryDate + "/", 0755, function( err ) {
                 if( err ) {
                     errorResponse( res, err );
                     throw err;
                 }
-                var util  = require('util'),
-                spawn = require('child_process').spawn,
-                grunt = spawn( "./node_modules/grunt-cli/bin/grunt",["--verbose", "--base", dataDir, "--gruntfile", tempSaveDir + directoryDate + "/" + hash + ".js" ]);
-                //base should be where the files are, config is the grunt.js file
-                grunt.stdout.on('data', function (data) {
-                    console.log('stdout: ' + data);
-                });
+                //Begin ReadFile
+                fs.readFile( "gruntbase.js","utf-8", function( err, data) {
+                    if( err ) {
+                        errorResponse( res, err );
+                    }
+                    //build replacement
+                    var replacement = "[ ";
+                    _.each( config.include, function( val, index, list ) {
+                        replacement += "'" + val + ".js'";
+                        if( (index+1) !== list.length ) {
+                            replacement += ", ";
+                        }
+                    });
 
-                grunt.stderr.on('data', function (data) {
-                    console.log('stderr: ' + data);
-                });
+                    replacement += "]";
+                    var temp = data.replace("\"@SRC@\"", replacement)
+                                   .replace("\"@DEST@\"", "'" + tempSaveDir + directoryDate + "/<%= pkg.name %>.custom.js'" )
+                                   .replace("\"@DESTIIFE@\"", "'" + tempSaveDir + directoryDate + "/aerogear.custom.js'" )
+                                   .replace( "\"@DESTMIN@\"",  "'" + tempSaveDir + directoryDate + "/<%= pkg.name %>.custom.min.js'" )
+                                   .replace( "\"@DESTSOURCEMAP@\"", "'" + tempSaveDir + directoryDate + "/<%= pkg.name %>.custom.map'" )
+                                   .replace( "\"@CONCAT@\"", "'" + repoDir + "node_modules/grunt-contrib-concat/tasks'")
+                                   .replace( "\"@UGLY@\"", "'" + repoDir + "node_modules/grunt-contrib-uglify/tasks'")
+                                   .replace( "\"@SOURCEMAPNAME@\"", "'<%= pkg.name %>.custom.min.js'")
+                                   .replace( "\"@SOURCEMAPPREFIX@\"", sourceMapPrefix );
+                    //write a new temp grunt file
+                    fs.writeFile( tempSaveDir + directoryDate + "/" + hash + ".js", temp, "utf8", function( err ) {
 
-                grunt.on('exit', function (code) {
-
-                    //Files are created, time to zip them up
-                    var archive = new zip();
-                    archive.addFiles([
-                        { name: "aerogear.custom.min.js", path: tempSaveDir + directoryDate + "/aerogear.custom.min.js" },
-                        { name: "aerogear.custom.js", path: tempSaveDir + directoryDate + "/aerogear.custom.js" },
-                        { name: "aerogear.custom.map", path: tempSaveDir + directoryDate + "/aerogear.custom.map" }
-                    ], function( err ) {
                         if( err ) {
                             errorResponse( res, err );
+                            throw err;
                         }
-                        var buff = archive.toBuffer();
+                        var util  = require('util'),
+                        spawn = require('child_process').spawn,
+                        grunt = spawn( "./node_modules/grunt-cli/bin/grunt",["--verbose", "--base", dataDir, "--gruntfile", tempSaveDir + directoryDate + "/" + hash + ".js" ]);
+                        //base should be where the files are, config is the grunt.js file
+                        grunt.stdout.on('data', function (data) {
+                            console.log('stdout: ' + data);
+                        });
 
-                        res.send( buff );
-                        //res.send( fs.readFileSync( dataDir + directoryDate + "/aerogear." + hash + ".min.js" ) );
-                        console.log('child process exited with code ' + code);
-                        //remove temp grunt file
+                        grunt.stderr.on('data', function (data) {
+                            console.log('stderr: ' + data);
+                        });
 
-                        rimraf( tempSaveDir + directoryDate + "/", function( err ) {
-                            if( err ) {
-                                console.log( err );
-                            }
+                        grunt.on('exit', function (code) {
+
+                            //Files are created, time to zip them up
+                            var archive = new zip();
+                            archive.addFiles([
+                                { name: "aerogear.custom.min.js", path: tempSaveDir + directoryDate + "/aerogear.custom.min.js" },
+                                { name: "aerogear.custom.js", path: tempSaveDir + directoryDate + "/aerogear.custom.js" },
+                                { name: "aerogear.custom.map", path: tempSaveDir + directoryDate + "/aerogear.custom.map" }
+                            ], function( err ) {
+                                if( err ) {
+                                    errorResponse( res, err );
+                                }
+                                var buff = archive.toBuffer();
+
+                                res.send( buff );
+                                //res.send( fs.readFileSync( dataDir + directoryDate + "/aerogear." + hash + ".min.js" ) );
+                                console.log('child process exited with code ' + code);
+                                //remove temp grunt file
+
+                                rimraf( tempSaveDir + directoryDate + "/", function( err ) {
+                                    if( err ) {
+                                        console.log( err );
+                                    }
+                                });
+                            });
                         });
                     });
                 });
+            //End ReadFIle
             });
+
+
+            function errorResponse( res, err ) {
+                res.status( 500 );
+                res.send( err );
+            }
         });
-//End ReadFIle
-    });
+    };
 
 
-    function errorResponse( res, err ) {
-        res.status( 500 );
-        res.send( err );
-    }
+    /**
+     *  Initializes the sample application.
+     */
+    self.initialize = function() {
+        self.setupVariables();
+        self.setupTerminationHandlers();
 
-});
+        // Create the express server and routes.
+        self.initializeServer();
+    };
 
-//OPENSHIFT Stuff
-//  Get the environment variables we need.
-var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP || "localhost";
-var port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
 
-if (typeof ipaddr === "undefined") {
-   console.warn('No OPENSHIFT_INTERNAL_IP environment variable');
-}
+    /**
+     *  Start the server (starts up the sample application).
+     */
+    self.start = function() {
+        //  Start the app on the specific interface (and port).
+        self.app.listen(self.port, self.ipaddress, function() {
+            console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), self.ipaddress, self.port);
+        });
+    };
 
-//  terminator === the termination handler.
-function terminator(sig) {
-   if (typeof sig === "string") {
-      console.log('%s: Received %s - terminating Node server ...',
-                  Date(Date.now()), sig);
-      process.exit(1);
-   }
-   console.log('%s: Node server stopped.', Date(Date.now()) );
-}
+};   /*  Sample Application.  */
 
-//  Process on exit and signals.
-process.on('exit', function() { terminator(); });
 
-// Removed 'SIGPIPE' from the list - bugz 852598.
-['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS',
- 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-].forEach(function(element, index, array) {
-    process.on(element, function() { terminator(element); });
-});
 
-//  And start the app on that interface (and port).
-app.listen(port, ipaddr, function() {
-   console.log('%s: Node server started on %s:%d ...', Date(Date.now() ),
-               ipaddr, port);
-});
+/**
+ *  main():  Main code.
+ */
+var zapp = new SampleApp();
+zapp.initialize();
+zapp.start();
 
